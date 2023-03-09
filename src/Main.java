fa82144cfa4d470511a5a029181b001af24e67ef
T00226260 <dquinny2003@gmail.com>
2023-03-09 17:10:31 +0000

    dsfds

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        public static void mergeSort(int[] a){
+            if (a.length <= 1) { return; }
+            int[] first = new int[a.length / 2];
+            int[] second = new int[a.length - first.length];
+
+
+            first = Arrays.copyOfRange(a,0, a.length / 2);
+            second = Arrays.copyOfRange(a, a.length / 2, a.length)
+
+
+           mergeSort(first);
+           mergeSort(second);
+           merge(first, second, a); }
+
+            }
+    public static void merge(int []first, int []second, int [] a) {
+
+        int iFirst = 0;
+        int iSecond = 0;
+        int j = 0;
+
+        while (iFirst < first.length && iSecond < second.length)
+            if (first[iFirst] < second[iSecond]) a[j] = first[iFirst]{
+
+        }
+
+
+//        increment iFirst else
+//
+//        a[j] = second[iSecond]
+//
+//        increment iSecond
+//
+//        increment j ENDWHILE
+
+    }
+
+
+        private static void mergeSort(int[] first) {
+
+    }
+}
---
+++ src/TestMyArrays.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMyArrays {
+
+    public static void main(String [] args){
+
+        int [] a = {1, 2, 3, 4, 5, 6};
+
+        System.out.print("Array Test:" + a.toString());
+
+
+    }
+
+}
